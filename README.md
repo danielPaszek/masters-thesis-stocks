733fe2e37db5fd0f84153648289530a7738481b8
danielPaszek <daniel.paszek@student.put.poznan.pl>
2025-04-18 20:32:50 +0200

    alpha for relative

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 Python 3.12
 
+./data:
+- data - responses from API
+- error - failed responses
+- error-data - data we got for failed tickers
+- final-data-absolute - includes alpha, yPrices and absolute values of ratios
+- final-data-relative - includes alpha, yPrices and relative values of ratios
+- market-data - dividends from API
+- random - ...
+- results-no-alpha - combine prices and dividends sources
+- temp
+- transformed - mapped from responses to parceable form
+- y-no-alpha - calculated yPrices
--- analysis/generate_all.py
+++ analysis/generate_all.py
@@ -4,18 +4,22 @@ import json
 import os
 import csv
 from utils.generateFinalRow import generateFinalRows
+
 # from utils.generateRelativeRow import getRelativeRows
 
 
-ratioKeys = ['psRatio', 'peRatio', 'priceToBook', 'evToEbitda', 'evToEbit', 'priceToFreeCashFlow', 'evToSales', 'evToGrossProfit', 'priceToGrossProfit']
+ratioKeys = ['psRatio', 'peRatio', 'priceToBook', 'evToEbitda', 'evToEbit', 'priceToFreeCashFlow', 'evToSales',
+             'evToGrossProfit', 'priceToGrossProfit']
 yKeys = ['yAdjustedPriceSpinoffExcl1Year', 'yAdjustedPrice1Year', 'yPrice1Year']
 y2Keys = ['yAdjustedPriceSpinoffExcl2Year', 'yAdjustedPrice2Year', 'yPrice2Year']
 numericalKeys = ratioKeys + yKeys + y2Keys
 
 resultsPath = '../data/y-no-alpha/'
+pathEqual = '../data/equal-weight-spdr-$.json'
+pathSpdr = '../data/spdr-$.json'
 
 
-def mapFinalRowToCompanyJson(companyDf: pd.DataFrame):
+def mapDfToCompanyJson(companyDf: pd.DataFrame):
     res = {}
     for index, row in companyDf.iterrows():
         splitedDate = row['date'].split('-')
@@ -27,6 +31,7 @@ def mapFinalRowToCompanyJson(companyDf: pd.DataFrame):
         res[year][month] = row.to_dict()
     return res
 
+
 def mapCompanyJsonToTemp(companyData):
     res = []
     for year, yearData in companyData.items():
@@ -34,47 +39,39 @@ def mapCompanyJsonToTemp(companyData):
             res.append(monthData)
     return res
 
-def generateFinalAbsoluteData():
-    pathEqual = '../data/equal-weight-spdr-$.json'
-    pathSpdr = '../data/spdr-$.json'
-
-    # load both year files
-
-
-    fileNames = os.listdir(resultsPath)
-    companyDf = pd.DataFrame()
-    i = 0
+def generateFinalRelativeDataFromTemp(tempPath):
+    allData = []
+    relativeDf = pd.read_csv(tempPath, sep='\t')
     offsets = [1, 2]
-    dataEqual = [0]
-    dataSpdr = [0]
-    for offset in offsets:
-        json_data = open(pathEqual.replace('$', str(offset)))
-        dataEqual.append(json.load(json_data))
-        json_data = open(pathSpdr.replace('$', str(offset)))
-        dataSpdr.append(json.load(json_data))
+    dataEqual, dataSpdr = loadSpyData(offsets, pathEqual, pathSpdr)
+
+    dfs = {ticker: data for ticker, data in relativeDf.groupby('ticker')}
+    for fileName, data in dfs.items():
+        companyData = mapDfToCompanyJson(data)
+        generateAlphaForCompanyJson(companyData, dataEqual, dataSpdr, offsets)
+        toTempCompany = mapCompanyJsonToTemp(companyData)
+        for tempData in toTempCompany:
+            allData.append(tempData)
+        with open('../data/final-data-relative/' + fileName.replace('.csv', '.json'), 'w') as f:
+            json.dump(companyData, f)
+            print('Done' + fileName)
+
+    with open('../data/temp-relative-alpha.csv', 'w', newline='') as myfile:
+        dict_writer = csv.DictWriter(myfile, allData[0].keys())
+        dict_writer.writeheader()
+        dict_writer.writerows(allData)
+
+def generateFinalAbsoluteData():
+    fileNames = os.listdir(resultsPath)
+    offsets = [1, 2]
+    dataEqual, dataSpdr = loadSpyData(offsets, pathEqual, pathSpdr)
 
     allData = []
     for fileName in fileNames:
-        # companyDf, i = generateFinalRows(data, ratioKeys, fileName, i, companyDf, offsets) y-no-alpha is this
         companyDf = pd.read_csv(resultsPath + fileName, delimiter='\t')
-        companyData = mapFinalRowToCompanyJson(companyDf)
+        companyData = mapDfToCompanyJson(companyDf)
 
-        for year, yearData in companyData.items():
-            for month, monthData in yearData.items():
-                for offset in offsets:
-                    yearSpy = dataSpdr[offset].get(year, 0)
-                    monthSpy = yearSpy.get(month, 0) if yearSpy else None
-                    if not monthSpy:
-                        continue
-                    yearEqual = dataEqual[offset].get(year, 0)
-                    monthEqual = yearEqual.get(month, 0) if yearEqual else None
-                    if not monthEqual:
-                        continue
-                    monthData['alpha'+str(offset)+'Year'] = monthData['yPrice'+str(offset)+'Year'] - monthSpy['yPrice']
-                    monthData['adjustedAlpha'+str(offset)+'Year'] = monthData['yAdjustedPrice'+str(offset)+'Year'] - monthSpy['yAdjustedTotalPrice']
-
-                    monthData['equalAlpha'+str(offset)+'Year'] = monthData['yPrice'+str(offset)+'Year'] - monthEqual['yPrice'+str(offset)+'Year']['mean']
-                    monthData['equalAdjustedAlpha'+str(offset)+'Year'] = monthData['yAdjustedPrice'+str(offset)+'Year'] - monthEqual['yAdjustedPrice'+str(offset)+'Year']['mean']
+        generateAlphaForCompanyJson(companyData, dataEqual, dataSpdr, offsets)
 
         toTempCompany = mapCompanyJsonToTemp(companyData)
         for tempData in toTempCompany:
@@ -88,6 +85,39 @@ def generateFinalAbsoluteData():
         dict_writer.writeheader()
         dict_writer.writerows(allData)
 
+
+def generateAlphaForCompanyJson(companyData, dataEqual, dataSpdr, offsets):
+    for year, yearData in companyData.items():
+        for month, monthData in yearData.items():
+            for offset in offsets:
+                yearSpy = dataSpdr[offset].get(year, 0)
+                monthSpy = yearSpy.get(month, 0) if yearSpy else None
+                if not monthSpy:
+                    continue
+                yearEqual = dataEqual[offset].get(year, 0)
+                monthEqual = yearEqual.get(month, 0) if yearEqual else None
+                if not monthEqual:
+                    continue
+                monthData['alpha' + str(offset) + 'Year'] = monthData['yPrice' + str(offset) + 'Year'] - monthSpy['yPrice']
+                monthData['adjustedAlpha' + str(offset) + 'Year'] = monthData['yAdjustedPrice' + str(offset) + 'Year'] - monthSpy['yAdjustedTotalPrice']
+
+                monthData['equalAlpha' + str(offset) + 'Year'] = monthData['yPrice' + str(offset) + 'Year'] - monthEqual['yPrice' + str(offset) + 'Year']['mean']
+                monthData['equalAdjustedAlpha' + str(offset) + 'Year'] = monthData['yAdjustedPrice' + str(offset) + 'Year'] - monthEqual['yAdjustedPrice' + str(offset) + 'Year']['mean']
+
+
+def loadSpyData(offsets, pathEqual, pathSpdr):
+    # offset starts at 1
+    dataEqual = [0]
+    dataSpdr = [0]
+    for offset in offsets:
+        json_data = open(pathEqual.replace('$', str(offset)))
+        dataEqual.append(json.load(json_data))
+        json_data = open(pathSpdr.replace('$', str(offset)))
+        dataSpdr.append(json.load(json_data))
+    return dataEqual, dataSpdr
+
+
 if __name__ == "__main__":
     generateFinalAbsoluteData()
-
+    tempPath = '../data/temp-relative.csv'
+    generateFinalRelativeDataFromTemp(tempPath)
